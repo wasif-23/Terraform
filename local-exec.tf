42f49058224f34843bd8a381b5b3c5c33e30119c
wasif-23 <83427545+wasif-23@users.noreply.github.com>
2021-05-18 16:07:35 +0530

    Delete local-exec.tf

--- local-exec.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "myec2" {
-  ami = "ami-0518bb0e75d3619ca"
-  instance_type = "t2.micro"
-  key_name = "myec2keypair"
-
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.myec2.private_ip} >> private_ips.txt"
-  }
-}
